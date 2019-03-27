2fd920e34980e09b40848e6b08e3a2bd52c86312
Meghamsh Gurram <meghamshgurram@Meghamshs-MacBook-Pro.local>
2019-03-26 22:11:15 -0400

    Eureka Naming server implementation

--- currency-conversion-services/src/main/java/com/meg/microservices/currencyconversionservices/CurrencyConversionServicesApplication.java
+++ currency-conversion-services/src/main/java/com/meg/microservices/currencyconversionservices/CurrencyConversionServicesApplication.java
@@ -2,14 +2,16 @@ package com.meg.microservices.currencyconversionservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.meg.microservices.currencyconversionservices")
+@EnableDiscoveryClient
 public class CurrencyConversionServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CurrencyConversionServicesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConversionServicesApplication.class, args);
+    }
 
 }
